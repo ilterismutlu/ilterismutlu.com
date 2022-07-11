22b22458e10701af011de7126536dcb0f671330f
İlteriş MUTLU <ilterismutlu@gmail.com>
2022-07-11 18:46:39 +0300

    kod revize
    ok

--- DWH Hata Çözümleri/Exchange hatası/ORA-14097 - EXCHANGE PARTITION Hatası Test datası oluşturma - Çözüm.sql
+++ DWH Hata Çözümleri/Exchange hatası/ORA-14097 - EXCHANGE PARTITION Hatası Test datası oluşturma - Çözüm.sql
@@ -1,3 +1,4 @@
+
 --DROP TABLE tbl_customer_hist PURGE;                              
     
 CREATE TABLE tbl_customer_hist  NOLOGGING PARALLEL (DEGREE 8 INSTANCES 1) COMPRESS 
@@ -52,10 +53,19 @@ SELECT d.as_of_date,
 --SELECT * FROM tbl_customer_hist ORDER BY 1,2,3;
 
 --SELECT COUNT(*) FROM tbl_customer_hist;
---3103100
+
 
 --1) exchange partition HATALARI: 
+--ORA-14099: all rows in table do not qualify for specified partition
 
+--DROP TABLE tbl_customer_hist_exc PURGE;
+
+CREATE TABLE tbl_customer_hist_exc NOLOGGING PARALLEL ( DEGREE 8 INSTANCES 1 ) COMPRESS AS
+SELECT as_of_date+1 as_of_date, customer_id, branch_id, c_name, adres, balance 
+  FROM tbl_customer_hist
+ WHERE as_of_date = TO_DATE('20220131','YYYYMMDD');
+ 
+ 
 --ORA-14097: column type or size mismatch in ALTER TABLE EXCHANGE PARTITION
 --case 1:
 ALTER TABLE tbl_customer_hist_exc MODIFY c_name VARCHAR2(22 CHAR);
@@ -70,3 +80,74 @@ COMMIT;  --ROLLBACK; de olabilir.
 ALTER TABLE tbl_customer_hist
    EXCHANGE PARTITION FOR (TO_DATE('20220201','YYYYMMDD'))
  WITH TABLE tbl_customer_hist_exc;
+
+--gözlem:
+SELECT table_name,column_name, data_type, data_length, data_precision, data_scale, nullable, column_id, character_set_name, char_col_decl_length, char_length, char_used, default_on_null, identity_column
+  FROM user_tab_columns 
+ WHERE 1=1
+       AND table_name IN ( 'TBL_CUSTOMER_HIST','TBL_CUSTOMER_HIST_EXC')
+       AND column_id IN (
+                            SELECT column_id
+                              FROM (
+                                    (
+                                     SELECT data_type, data_length, data_precision, data_scale, nullable, column_id, character_set_name, char_col_decl_length, char_length, char_used, default_on_null, identity_column
+                                       FROM user_tab_columns where table_name ='TBL_CUSTOMER_HIST_EXC'
+                                     MINUS
+                                     SELECT data_type, data_	length, data_precision, data_scale, nullable, column_id, character_set_name, char_col_decl_length, char_length, char_used, default_on_null, identity_column
+                                       FROM user_tab_columns where table_name='TBL_CUSTOMER_HIST'
+                                    )
+								    UNION ALL
+								    (
+                                     SELECT data_type, data_length, data_precision, data_scale, nullable, column_id, character_set_name, char_col_decl_length, char_length, char_used, default_on_null, identity_column
+                                       FROM user_tab_columns where table_name ='TBL_CUSTOMER_HIST'
+                                     MINUS
+                                     SELECT data_type, data_length, data_precision, data_scale, nullable, column_id, character_set_name, char_col_decl_length, char_length, char_used, default_on_null, identity_column
+                                       FROM user_tab_columns where table_name='TBL_CUSTOMER_HIST_EXC'
+                                    )
+                                   )
+                          )
+ORDER BY column_id;
+
+--çözüm:
+ALTER TABLE tbl_customer_hist MODIFY c_name varchar2(22 CHAR);
+
+ALTER TABLE tbl_customer_hist
+  EXCHANGE PARTITION FOR (TO_DATE('20220201','YYYYMMDD'))
+  WITH TABLE tbl_customer_hist_exc;
+--Table altered  
+  
+  
+--ORA-14097: column type or size mismatch in ALTER TABLE EXCHANGE PARTITION
+--case 2:
+
+ALTER TABLE tbl_customer_hist_exc ADD dummy_sutun NUMBER;
+----ALTER TABLE tbl_customer_hist_exc SET UNUSED COLUMN dummy_sutun;  --exadata kurulu olmadığı için, konunun anlaşılması için alttaki drop'u yapmayıp, bu şekilde simüle ettim. 
+ALTER TABLE tbl_customer_hist_exc drop COLUMN dummy_sutun;
+--add+drop column yapiliyor. Distan görüntü ayni gibi gözükse de exchange'de hata aliyor.
+
+ALTER TABLE tbl_customer_hist
+  EXCHANGE PARTITION FOR (TO_DATE('20220201','YYYYMMDD'))
+  WITH TABLE tbl_customer_hist_exc;
+--ORA-14097: column type or size mismatch in ALTER TABLE EXCHANGE PARTITION  
+  
+SELECT * 
+  FROM dba_unused_col_tabs 
+ WHERE 1=1 AND table_name IN ( 'TBL_CUSTOMER_HIST','TBL_CUSTOMER_HIST_EXC');
+--OWNER	      TABLE_NAME	       COUNT
+--C##USER2	TBL_CUSTOMER_HIST_EXC	1
+
+
+--case 2 çözüm:
+ALTER TABLE tbl_customer_hist_exc NOCOMPRESS;
+ALTER TABLE tbl_customer_hist_exc DROP UNUSED COLUMNS;
+ALTER TABLE tbl_customer_hist_exc MOVE PARALLEL 4 NOLOGGING;
+
+SELECT * 
+  FROM dba_unused_col_tabs 
+ WHERE 1=1 AND table_name IN ( 'TBL_CUSTOMER_HIST','TBL_CUSTOMER_HIST_EXC');
+--null
+
+ALTER TABLE tbl_customer_hist
+  EXCHANGE PARTITION FOR (TO_DATE('20220201','YYYYMMDD'))
+  WITH TABLE tbl_customer_hist_exc;
+--Table altered  
